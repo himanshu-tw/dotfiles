4c9fbf64c55eec52cafe84adf672194bee3af5f8
Himanshu Tiwari <ht5620085@gmail.com>
2026-05-07 13:29:50 +0530

    more changes

--- install.sh
+++ install.sh
@@ -1,61 +1,106 @@
 #!/bin/bash
 
+set -e
+
 DOTFILES="$HOME/dotfiles"
 
-install_if_missing() {
-  if ! command -v $1 &> /dev/null; then
-    echo "Installing $1..."
-    $2
-  else
-    echo "$1 already installed, skipping."
-  fi
-}
+echo "==> Starting dotfiles setup..."
 
 # System packages
-sudo apt update
-sudo apt install -y git curl unzip zsh tmux ripgrep fd-find fzf bat eza
+echo "==> Installing system packages..."
+sudo apt update -qq
+sudo apt install -y git curl unzip zsh tmux ripgrep fd-find fzf bat eza neovim alacritty
 
 # Oh My Zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "==> Installing Oh My Zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
-  echo "oh-my-zsh already installed, skipping."
+  echo "==> Oh My Zsh already installed, skipping."
 fi
 
-# Plugins
+# Zsh plugins
+echo "==> Installing Zsh plugins..."
 [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] && \
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] && \
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone -q https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Starship
-install_if_missing starship "curl -sS https://starship.rs/install.sh | sh"
+if ! command -v starship &> /dev/null; then
+  echo "==> Installing Starship..."
+  curl -sS https://starship.rs/install.sh | sh -s -- -y
+else
+  echo "==> Starship already installed, skipping."
+fi
 
 # Mise
-install_if_missing mise "curl https://mise.run | sh"
+if ! command -v mise &> /dev/null; then
+  echo "==> Installing Mise..."
+  curl https://mise.run | sh
+else
+  echo "==> Mise already installed, skipping."
+fi
 
 # Atuin
-install_if_missing atuin "curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh"
+if ! command -v atuin &> /dev/null; then
+  echo "==> Installing Atuin..."
+  curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+else
+  echo "==> Atuin already installed, skipping."
+fi
 
 # Zoxide
-install_if_missing zoxide "curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh"
+if ! command -v zoxide &> /dev/null; then
+  echo "==> Installing Zoxide..."
+  curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+else
+  echo "==> Zoxide already installed, skipping."
+fi
 
-# Alacritty
-install_if_missing alacritty "sudo apt install -y alacritty"
+# Nerd Font
+if ! fc-list | grep -qi "JetBrainsMono Nerd Font"; then
+  echo "==> Installing JetBrainsMono Nerd Font..."
+  wget -q https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip
+  mkdir -p ~/.local/share/fonts/JetBrainsMono
+  unzip -q /tmp/JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono
+  fc-cache -fv > /dev/null
+  rm /tmp/JetBrainsMono.zip
+else
+  echo "==> JetBrainsMono Nerd Font already installed, skipping."
+fi
 
-# Neovim
-install_if_missing nvim "sudo apt install -y neovim"
+# Tmux Plugin Manager
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  echo "==> Installing TPM..."
+  git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+  echo "==> TPM already installed, skipping."
+fi
+
+# Default shell to zsh
+if [ "$SHELL" != "$(which zsh)" ]; then
+  echo "==> Changing default shell to zsh..."
+  chsh -s $(which zsh)
+fi
 
 # Symlinks
+echo "==> Creating symlinks..."
 mkdir -p ~/.config/alacritty
+
+# Remove existing to avoid nesting issue
+rm -rf ~/.config/nvim
+rm -f ~/.config/alacritty/alacritty.toml
+rm -f ~/.zshrc
+rm -f ~/.config/starship.toml
+rm -f ~/.tmux.conf
+
 ln -sf $DOTFILES/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf $DOTFILES/zsh/.zshrc ~/.zshrc
 ln -sf $DOTFILES/starship/starship.toml ~/.config/starship.toml
 ln -sf $DOTFILES/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES/nvim ~/.config/nvim
 
-# git ssh key setup
-#./git-ssh/github-ssh-setup.sh
-
-echo "Done."
+echo ""
+echo "==> Done! Restart terminal or run: exec zsh"
